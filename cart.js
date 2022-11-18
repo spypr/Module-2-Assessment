94fd80a82bdb95ffc07069d8b5fffdc26389a2fe
Steven Pyper <steven.pyper@gmail.com>
2022-11-18 10:56:10 -0700

    uploading assessment 2 - Steve

---
+++ cart.js
@@ -0,0 +1,100 @@
+///////////////////////////////////////////////
+///////////////////CART.JS/////////////////////
+///////////////////////////////////////////////
+/*
+    In this file, you'll be writing code to
+    calculate order totals. You'll also be 
+    creating customer objects.  
+*/
+
+
+//////////////////PROBLEM 1////////////////////
+/*  
+    Below is a cart array that has food objects
+    inside. 
+
+    Write a callback below that uses the reduce
+    array method to calculate the sum of all
+    the food. 
+*/
+
+const cart = [
+    {
+        name: 'pizza', 
+        price: 9.99
+    }, 
+    {
+        name: 'pasta', 
+        price: 8.99
+    }, 
+    {
+        name: 'salad', 
+        price: 7.99
+    }
+]
+
+//CODE HERE
+
+const summedPrice = cart.reduce(function(acc,elem) {
+    return acc + elem.price
+}, 0)
+
+console.log(summedPrice)
+
+//////////////////PROBLEM 2////////////////////
+/*  
+    Write a function called `calcFinalPrice` that
+    can take in `cartTotal`,`couponValue`,
+    and `tax` arguments. 
+
+    Inside the function, calculate the tax 
+    on the cartTotal and add it in. Subtract
+    the value of the coupon. Return the final
+    number. 
+
+    Note: the numbers passed in for `tax` will be
+    decimals, for example: .06 for a 6% tax.
+*/
+
+//CODE HERE
+
+const calcFinalPrice = 
+
+//////////////////PROBLEM 3////////////////////
+/*  
+    In this problem, you'll create a model for 
+    a customer object as well as an example
+    object. 
+
+    Plan out a customer object for the cart page.
+    Think about the information that a 
+    restaurant would need about its customers.
+
+    In the TEXT ANSWER area below, describe the
+    properties that your customer object will have
+    and why you chose those properties.
+
+    Explain what data types each property should be
+    and why you chose those data types. 
+
+    Your object should have at least 4 properties. 
+*/
+
+
+    "We will need to know the Customer's first and last name, their cell phone number incase we can't find their address or need to send delivery updates, we will also need their full address, and payment information to complete payment for their purchase."
+
+
+
+/*
+    Now, create a customer object following your own
+    guidelines.
+*/
+
+const customer = {
+    firstName: 'Bob',
+    lastName: 'Smith',
+    street: '123 Main St',
+    city: 'Seattle',
+    cellPhoneNumber: 80145665456,
+    paymentType: ['credit card', 'debit card']
+}
